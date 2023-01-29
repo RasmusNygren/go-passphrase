1a091ad5c214459ba57079eb1365d7c6fed10b61
Rasmus Nygren <rasmus.nygren99@gmail.com>
2023-01-29 14:52:20 +0100

    Gofmt

--- passphrase/generator.go
+++ passphrase/generator.go
@@ -3,91 +3,90 @@ package passphrase
 import (
 	"bufio"
 	"crypto/rand"
+	"io/fs"
 	"math/big"
 	"os"
-    "io/fs"
 	"strings"
 )
 
 // TODO: Better error handling
 
 type WordList struct {
-    digits int 
-    table map[string]string
+	digits int
+	table  map[string]string
 }
 
-
 func NewWordList(path string, digits int) *WordList {
-    cwd, _ := os.Getwd()
-    f := os.DirFS(cwd)
-    return NewWordListCustomFS(f, path, digits)
+	cwd, _ := os.Getwd()
+	f := os.DirFS(cwd)
+	return NewWordListCustomFS(f, path, digits)
 
 }
 
 func NewWordListCustomFS(f fs.FS, path string, digits int) *WordList {
-    table := make(map[string]string)
+	table := make(map[string]string)
 
-    readFile, err := f.Open(path)
-    if err != nil {
-        panic(err)
-    }
+	readFile, err := f.Open(path)
+	if err != nil {
+		panic(err)
+	}
 
-    // TODO: Implement error handling. This should not be deferred without
-    // error handling, must be checked explicitly.
-    defer readFile.Close()
+	// TODO: Implement error handling. This should not be deferred without
+	// error handling, must be checked explicitly.
+	defer readFile.Close()
 
-    fileScanner := bufio.NewScanner(readFile)
+	fileScanner := bufio.NewScanner(readFile)
 
-    for fileScanner.Scan() {
-        line := fileScanner.Text()
-        splits := strings.Fields(line)
-        key, val := splits[0], splits[1]
-        table[key] = val
-    }
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		splits := strings.Fields(line)
+		key, val := splits[0], splits[1]
+		table[key] = val
+	}
 
-    wordList := WordList{
-        digits: digits,
-        table: table,
-    }
-    return &wordList
+	wordList := WordList{
+		digits: digits,
+		table:  table,
+	}
+	return &wordList
 }
 
 func generateWord(wl *WordList) string {
-    wordList := wl.table
-    digits := wl.digits
+	wordList := wl.table
+	digits := wl.digits
 
-    var stringBuilder strings.Builder
-    for i := 0; i < digits; i++ {
-        // This assumes a 6-sided die.
-        randint, err := rand.Int(rand.Reader, big.NewInt(6))
-        if err != nil {
-            panic(err)
-        }
+	var stringBuilder strings.Builder
+	for i := 0; i < digits; i++ {
+		// This assumes a 6-sided die.
+		randint, err := rand.Int(rand.Reader, big.NewInt(6))
+		if err != nil {
+			panic(err)
+		}
 
-        // This is safe as the generated numbers are always in [1,6]
-        randint = randint.Add(randint, big.NewInt(1))
-        stringBuilder.WriteString(randint.String())
-    }
+		// This is safe as the generated numbers are always in [1,6]
+		randint = randint.Add(randint, big.NewInt(1))
+		stringBuilder.WriteString(randint.String())
+	}
 
-    randomNumberAsStr := stringBuilder.String()
-    // fmt.Println(randomNumberAsStr)
-    // fmt.Println(wordList[randomNumberAsStr])
-    word := wordList[randomNumberAsStr]
-    return word
+	randomNumberAsStr := stringBuilder.String()
+	// fmt.Println(randomNumberAsStr)
+	// fmt.Println(wordList[randomNumberAsStr])
+	word := wordList[randomNumberAsStr]
+	return word
 }
 
 func GeneratePhraseWithCustomDelimiter(wl *WordList, length int, delimiter string) string {
-    var sb strings.Builder
-    for i := 0; i < length; i++ {
-        word := generateWord(wl)
-        sb.WriteString(word)
-        if i < length - 1 {
-            sb.WriteString(delimiter)
-        }
-    }
-    return sb.String()
+	var sb strings.Builder
+	for i := 0; i < length; i++ {
+		word := generateWord(wl)
+		sb.WriteString(word)
+		if i < length-1 {
+			sb.WriteString(delimiter)
+		}
+	}
+	return sb.String()
 }
 
 func GeneratePhrase(wl *WordList, length int) string {
-    return GeneratePhraseWithCustomDelimiter(wl, length, "-")
+	return GeneratePhraseWithCustomDelimiter(wl, length, "-")
 }
--- passphrase/wordlists.go
+++ passphrase/wordlists.go
@@ -1,17 +1,16 @@
 package passphrase
 
-
 import "embed"
 
 //go:embed wordlists/*
 var f embed.FS
 
 func EffLarge() *WordList {
-    wl := NewWordListCustomFS(f, "wordlists/eff_large_wordlist.txt", 5)
-    return wl
+	wl := NewWordListCustomFS(f, "wordlists/eff_large_wordlist.txt", 5)
+	return wl
 }
 
 func EffSmallShortWords() *WordList {
-    wl := NewWordListCustomFS(f, "wordlists/eff_short_wordlist_1.txt", 4)
-    return wl
+	wl := NewWordListCustomFS(f, "wordlists/eff_short_wordlist_1.txt", 4)
+	return wl
 }
