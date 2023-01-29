47b2135d891a108c9c3225c0b2d7ad353810a34a
Rasmus Nygren <rasmus.nygren99@gmail.com>
2023-01-29 11:33:58 +0100

    Added short word list

---
+++ wordlists.go
@@ -0,0 +1,11 @@
+package main
+
+func EffLarge() *WordList {
+    wl := NewWordList("./wordlists/eff_large_wordlist.txt", 5)
+    return wl
+}
+
+func EffSmallShortWords() *WordList {
+    wl := NewWordList("./wordlists/eff_short_wordlist_1.txt", 4)
+    return wl
+}
